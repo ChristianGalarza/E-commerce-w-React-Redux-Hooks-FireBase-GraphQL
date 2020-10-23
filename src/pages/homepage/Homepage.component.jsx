be9aa794825d9b97e5e5fb877aa1f7ac8b6576db
unknown <christianglrza99@gmail.com>
2020-10-23 14:23:25 -0700

    Añadiendo estilos y separando componentes del homepage

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component'
+
+import './homepage.styles.scss'
+
+const Homepage = () =>(
+    <div className='homepage'>
+        <DirectoryMenu />
+    </div>
+)
+
+export default Homepage
